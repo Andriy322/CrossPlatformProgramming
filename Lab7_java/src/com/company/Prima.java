6a37d2b0b2c99e59626e8ef574c0fe14d48c56ce
Andrii Fostiak <Ofrosti228@gmail.com>
2021-11-29 11:41:25 +0200

    Lab 7 java

---
+++ Lab7_java/src/com/company/Prima.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Prima implements Runnable {
+
+    int start;
+    Graph graph;
+    Map<Integer, Set<Connection>> result;
+    ReentrantLock lock = new ReentrantLock();
+
+    Prima(int s, Graph data) {
+        start = s;
+        graph = data;
+        result = new HashMap<>();
+    }
+
+    public class ComparatorConnection implements java.util.Comparator<Connection> {
+        @Override
+        public int compare(Connection o1, Connection o2) {
+            return Integer.compare(o1.weight, o2.weight);
+        }
+    }
+
+    @Override
+    public void run() {
+        result.put(start, new HashSet<>());
+        List<Connection> search;
+        Set<Connection> f = graph.Mark(start);
+
+        if (f == null) {
+            return;
+        } else {
+            search = new ArrayList<>(f);
+        }
+
+        Collections.sort(search, new ComparatorConnection());
+
+        for (int p = 0; p < search.size(); ++p) {
+            if (!result.containsKey(search.get(p).nameTo)) {
+                Set<Connection> n = graph.Mark(search.get(p).nameTo);
+                if (n != null) {
+                    List<Connection> connectSearch = new ArrayList<>(n);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    lock.lock();
+                    try {
+                        for (int ns = 0; ns < connectSearch.size(); ++ns) {
+                            if (connectSearch.get(ns).weight == search.get(p).weight) {
+                                var temp = connectSearch.get(ns).nameTo;
+                                var t = result.containsKey(Integer.valueOf(temp));
+                                if (!t)
+                                    break;
+                                var connections = result.get(Integer.valueOf(temp));
+                                if (connections == null)
+                                    break;
+                                connections.add(new Connection(search.get(p).nameTo, search.get(p).weight));
+                                result.put(search.get(p).nameTo, new HashSet<>());
+                                result.get(search.get(p).nameTo).add(new Connection(connectSearch.get(ns).nameTo, search.get(p).weight));
+                                break;
+                            }
+                        }
+                        search.addAll(n);
+                        Collections.sort(search, new ComparatorConnection());
+                        p = 0;
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ folder/Lab7/src/com/company/Main.java
@@ -0,0 +1,105 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\DELL\\IdeaProjects\\CrossPlatformProgramming\\CrossPlatformProgramming\\Lab7\\src\\Resources\\graph1.txt"));
+
+        String sCurrentLine;
+        Map<Integer, Set<Connection>> graphMap = new HashMap<>();
+
+        for (int i = 0; (sCurrentLine = br.readLine()) != null; ++i) {
+            String[] arrOfStr = sCurrentLine.split("=");
+            Set<Connection> l = new HashSet<>();
+
+            for(int s = 2; s < arrOfStr.length; s+=3){
+                l.add(new Connection( Integer.parseInt(arrOfStr[s]), Integer.parseInt(arrOfStr[s+1])));
+            }
+
+            graphMap.put(Integer.parseInt(arrOfStr[0]), l);
+        }
+        System.out.println("Input graph looks like this:");
+
+        Graph.printGraph(graphMap);
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Run multithreading?(y/n): ");
+            if(scanner.next().equals("n")){
+                Graph graph = new Graph(new HashMap<>(graphMap));
+                Dijkstra start0 = new Dijkstra(0, graph);
+                Thread Dijkstra0 = new Thread(start0);
+                Monitor mest0 = new Monitor(Dijkstra0);
+                mest0.start();
+                mest0.join();
+
+                double timeElapsed = System.currentTimeMillis();
+                Dijkstra0.start();
+                Dijkstra0.join();
+                timeElapsed -= System.currentTimeMillis();
+                System.out.println(start0.result.size());
+                System.out.println("Elapsed time - " + Math.abs(timeElapsed) + " milliseconds.");
+                System.out.println("Result graph: ");
+                Graph.printGraph(start0.result);
+            } else {
+                Graph graph = new Graph(new HashMap<>(graphMap));
+
+                Dijkstra Dijkstra1 = new Dijkstra(0, graph);
+                Dijkstra Dijkstra2 = new Dijkstra(1, graph);
+
+                Thread DijkstraThread1 = new Thread(Dijkstra1);
+                Thread DijkstraThread2 = new Thread(Dijkstra2);
+
+                Monitor monitor1 = new Monitor(DijkstraThread1);
+                Monitor monitor2 = new Monitor(DijkstraThread2);
+
+                monitor1.start();
+                monitor1.join();
+                monitor2.start();
+                monitor2.join();
+
+                double timeElapsed = System.currentTimeMillis();
+
+                DijkstraThread1.start();
+                DijkstraThread2.start();
+                DijkstraThread1.join();
+                DijkstraThread2.join();
+
+                timeElapsed -= System.currentTimeMillis();
+                System.out.println("Elapsed time: " + Math.abs(timeElapsed));
+
+                System.out.println("\nFirst thread graph: ");
+                Graph.printGraph(Dijkstra1.result);
+                System.out.println("\nSecond thread graph: ");
+                Graph.printGraph(Dijkstra2.result);
+
+                List<Connection> search = new ArrayList<>()     ;
+                Dijkstra1.result.forEach((k,v) -> {
+                    search.addAll(graphMap.get(k));
+                });
+
+                Comparator<Connection> comparatorName = (a, b) -> Integer.compare(a.weight, b.weight);
+                Collections.sort(search, comparatorName);
+
+                for(int i = 0; i < search.size(); ++i){
+                    if(!Dijkstra1.result.containsKey(search.get(i).nameTo)){
+                        List<Connection> connectSearch = new ArrayList<>(graphMap.get(search.get(i).nameTo));
+                        for(int ns = 0; ns < connectSearch.size(); ++ns){
+                            if(connectSearch.get(ns).weight == search.get(i).weight ) {
+                                Dijkstra1.result.get(connectSearch.get(ns).nameTo).add(new Connection(search.get(i).nameTo, search.get(i).weight));
+                                Dijkstra2.result.get(search.get(i).nameTo).add(new Connection(connectSearch.get(ns).nameTo, search.get(i).weight));
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                System.out.println("\nFinal graph: ");
+                Graph.printGraph(Dijkstra1.result);
+                Graph.printGraph(Dijkstra2.result);
+            }
+    }
+}
