9a7740e771af4ad80061369f1a593d5ee982ff39
Andrii Fostiak <Ofrosti228@gmail.com>
2021-12-01 12:26:08 +0200

    Correct lab 7

---
+++ Lab7/src/com/company/Connection.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Connection {
+    public int nameTo;
+    public int weight;
+
+    public Connection(int vertexTo, int weightOfConnection) {
+        nameTo = vertexTo;
+        weight = weightOfConnection;
+    }
+}
